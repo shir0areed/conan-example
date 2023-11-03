d3844ffa461400e12cd84c39509438f793ed1c4d
Anonymous <anonymous@example.com>
2023-11-03 10:30:55 +0900

    https://raw.githubusercontent.com/conan-io/examples2/main/tutorial/consuming_packages/conanfile_py/conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,12 @@
+from conan import ConanFile
+
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("zlib/1.2.11")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/3.22.6")
