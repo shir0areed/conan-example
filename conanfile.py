c9064b6ba5f9fa1a637f4c2ab2d40a66d670bc34
Anonymous <anonymous@example.com>
2023-11-03 10:48:52 +0900

    conan2でinstallしたときにCMakeUserPresets.jsonが生成されないように

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,9 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
 
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
         self.requires("zlib/1.2.11")
@@ -12,6 +11,14 @@ class CompressorRecipe(ConanFile):
     def build_requirements(self):
         self.tool_requires("cmake/[^3.23]")
         
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
